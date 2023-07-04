dfc130e8f0011577a13c705170630e56a96d4d58
litongjava <litongjava@qq.com>
2023-07-04 17:17:23 +0800

    整合 CustomTabs 支持内容复制

--- app/src/main/java/com/litongjava/web/view/chatgpt/study/MainActivity.java
+++ app/src/main/java/com/litongjava/web/view/chatgpt/study/MainActivity.java
@@ -2,12 +2,14 @@ package com.litongjava.web.view.chatgpt.study;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
 
 import com.litongjava.android.utils.AlertDialogUtils;
 import com.litongjava.android.utils.PermissionUtils;
@@ -24,17 +26,31 @@ public class MainActivity extends AppCompatActivity {
   private static final int permissionRequestCode = 10024;
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
-  @FindViewById(R.id.webview)
-  private WebView myWebView;
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getSupportActionBar().hide(); //隐藏ActionBar
     ViewInjectUtils.injectActivity(this, this);
 
-    initPermission();
 
+    initPermission();
+//    loadWebView();
+
+    String url = "https://chat.openai.com";
+    CustomTabsIntent intent = new CustomTabsIntent.Builder()
+      //隐藏地址栏
+      .setUrlBarHidingEnabled(true)
+//      .setShowTitle(false)
+      .build();
+    intent.launchUrl(MainActivity.this, Uri.parse(url));
+
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    // 在这里调用 finish() 来结束您的应用程序
+    finish();
   }
 
   private void initPermission() {
@@ -49,44 +65,13 @@ public class MainActivity extends AppCompatActivity {
         // If request is cancelled, the result arrays are empty.
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
           log.info("onRequestPermissionsResult granted");
-          loadWebView();
         } else {
           log.info("onRequestPermissionsResult denied");
           String message = "请前往设置->应用-><本应用名称>->权限中打开相关权限，否则部分功能无法正常运行！";
           AlertDialogUtils.showWaringDialog(this, message);
-          loadWebView();
         }
         return;
       }
     }
-  }
-
-  private void loadWebView() {
-    myWebView.loadUrl("https://chat.openai.com/");
-
-    WebSettings webSettings = myWebView.getSettings();
-    // 开启 JavaScript（如果需要的话）
-    webSettings.setJavaScriptEnabled(true);
-
-    // 开启 DOM storage API
-    webSettings.setDomStorageEnabled(true);
-
-    // 开启 database storage API
-    webSettings.setDatabaseEnabled(true);
-
-    // 设置缓存模式
-    webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
-
-    // 启用 WebView 内容访问
-    webSettings.setAllowContentAccess(true);
-
-    // 启用文件访问
-    webSettings.setAllowFileAccess(true);
-
-    // 启用剪贴板访问
-    webSettings.setAllowUniversalAccessFromFileURLs(true);
-    webSettings.setAllowFileAccessFromFileURLs(true);
-
-    myWebView.setWebViewClient(new WebViewClient());
   }
 }
